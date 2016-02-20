b75087747a7b417ea6d19c71ff6f9614c6c1337e
Anthony Halliday <anthony.halliday@outlook.com>
2016-02-20 10:35:53 +0000

    app start

---
+++ BeanFlick/App_Start/SocketConfig.js
@@ -0,0 +1,20 @@
+﻿
+var registerSockets = function (server) {
+
+    var io = require('socket.io')(server);
+
+    io.on('connection', function(socket) {
+        console.log('connected');
+
+        socket.on('chat message', function(msg) {
+            io.emit('chat message', msg);
+        });
+
+        socket.on('disconnect', function() {
+
+            console.log('disconnected');
+        });
+    });
+}
+
+module.exports = registerSockets;
