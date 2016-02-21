6acef17c9b9a4478538ebf24449da9b148e431a2
Matthew Care <matty_care@hotmail.co.uk>
2016-02-21 21:44:45 +0000

    Amending a conflict and adding rimraf to the package json

--- BeanFlick/public/min/scripts/factories/GlobalSettingsFactory.js
+++ BeanFlick/public/min/scripts/factories/GlobalSettingsFactory.js
@@ -1,1 +1,1 @@
-App.factory("GlobalSettingsFactory",function(){return{mouthData:{mouthLeft:{},mouthRight:{},width:200,height:100},mouthPositionY:400,mouthSpeed:10,mouthClosed:!1,throwableSize:{width:50},throwAreaHeight:200,throwableStartPosition:{x:400,y:800},friction:4}});
+App.factory("GlobalSettingsFactory",function(){return{mouthData:{mouthLeft:{},mouthRight:{},width:200,height:100},mouthPositionY:400,mouthSpeed:2,mouthClosed:!1,throwableSize:{width:50},throwAreaHeight:200,throwableStartPosition:{x:400,y:800},friction:4}});
