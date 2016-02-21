dd50c7e81aa577e2380b87735b8d04077f7f6a7a
Anthony Halliday <anthony.halliday@outlook.com>
2016-02-21 17:06:37 +0000

    Upload controller

--- BeanFlick/public/scripts/controllers/upload/UploadController.js
+++ BeanFlick/public/scripts/controllers/upload/UploadController.js
@@ -1,7 +1,48 @@
-﻿App.controller('UploadController', function ($scope, NotificationService, CurrentGameFactory, GameService) {
+﻿App.controller('UploadController', function ($scope, $element, NotificationService, CurrentGameFactory, GameService) {
 
     $scope.Enabled = false;
+    
+    $scope.MouthLeft = { X: 175, Y: 335 };
+    $scope.MouthRight = { X: 175, Y: 335 };
 
+    $scope.toggle = function(enabled) {
+        $scope.Enabled = enabled;
+    }
 
+    $scope.fileNameChanged = function(event) {
 
+        var reader = new FileReader();
+        reader.onload = function () {
+
+            $scope.Image = reader.result;
+            $scope.$apply();
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+
+    var pressedMarker = null;
+    $('.upload-preview').on('mousedown', '.marker', function(event) {
+        pressedMarker = $(event.target).data('id');
+        
+        document.addEventListener("mousemove" , $scope.mouseMove, false);
+        
+    });
+
+    $scope.mouseMove = function (event) {
+        var parentPosition = $('.upload-preview').position();
+
+        $scope[pressedMarker].X = event.pageX;// - parentPosition.left;
+        $scope[pressedMarker].Y = event.pageY;// - parentPosition.top;
+        var $elm = $('[data-id="' + pressedMarker + '"]');
+
+        $elm.css('left', $scope[pressedMarker].X + "px");
+        $elm.css('top', $scope[pressedMarker].Y + "px");
+        
+        document.addEventListener("mouseup" , $scope.eleMouseUp, false);
+    }
+
+    $scope.eleMouseUp = function (event) {
+        document.removeEventListener("mousemove" , $scope.mouseMove , false);
+        document.removeEventListener("mouseup" , $scope.eleMouseUp , false);
+    }
 });
